a6b43193897a4a508cfecb215c688dc7af7e6a6b
Leonardo Steffen <leosteffen84@gmail.com>
2016-10-26 17:37:10 -0700

    adds Hero object and 2way binding for hero name property

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,31 @@
 import { Component } from '@angular/core';
+import {  }
+
+
 @Component({
   selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
-    })
-    export class AppComponent { }
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}} </div>
+    <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name">
+    </div>`
+
+})
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
+}
+
+export class Hero {
+  id: number;
+  name: string;
+
+}
 
