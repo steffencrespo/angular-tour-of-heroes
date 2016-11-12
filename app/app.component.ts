0d2047ec45878612ceda3bf5d5cf4ec6e39a6bbe
Leonardo Steffen <leosteffen84@gmail.com>
2016-11-11 16:06:20 -0800

    part 3 - adds ngIf, (click) event binding, event handler method and [class.selected] property binding to apply style when a hero button when clicked

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,15 +7,19 @@ import { Component } from '@angular/core';
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes">
+      <li *ngFor="let hero of heroes" 
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}} </div>
-    <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
+    <div *ngIf="selectedHero">
+     <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+          <label>name: </label>
+          <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+      </div>
     </div>`,
     styles: [`
       .selected {
@@ -72,16 +76,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
 
 export class Hero {
   id: number;
   name: string;
-
 }
 
 const HEROES: Hero[] = [
@@ -96,5 +100,3 @@ const HEROES: Hero[] = [
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
 ];
-
-
