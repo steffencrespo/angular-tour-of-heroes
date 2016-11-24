0787dc32bf0796c14ae4b47fa310296a4c4a2867
Leonardo Steffen <leosteffen84@gmail.com>
2016-11-23 18:07:14 -0800

    part 5 - adds mock-heroes, hero.service and changes app.components to initialize and use the service appropriately

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,7 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+
+import { HeroService } from './hero.service';
 import { Hero } from './hero';
 
 
@@ -64,29 +67,27 @@ import { Hero } from './hero';
         margin-right: .8em;
         border-radius: 4px 0 0 4px;
       }
-  `]
+  `],
+  providers: [HeroService]
 
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-}
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+}
