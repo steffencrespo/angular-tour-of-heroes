e515ea4530317e3aaee78319bdb5eeae6869e2e0
Leonardo Steffen <leosteffen84@gmail.com>
2017-01-03 15:21:15 -0800

    part 6 - adds ngOnInit method to HeroDetailComponent; calls method to get hero from the service by id param taken from route; subscribes to route

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
+
 import { Location } from '@angular/common';
+import { Hero } from './hero';
 import { HeroService } from './hero.service';
 import 'rxjs/add/operator/switchMap';
 
@@ -25,4 +27,11 @@ export class HeroDetailComponent implements OnInit {
         private route: ActivatedRoute,
         private location: Location
     ) {}
+
+    ngOnInit(): void {
+        this.route.params
+            .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+            .subscribe(hero => this.hero = hero);
+    }
+
 }
