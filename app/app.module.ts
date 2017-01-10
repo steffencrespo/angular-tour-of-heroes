b37d1dc0551fc39369c82226c9a799ed7695d277
Leonardo Steffen <leosteffen84@gmail.com>
2017-01-09 16:19:15 -0800

    part 7 - removes mock-heroes and adds data service api, in-memory db with heroes data

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
@@ -16,7 +19,8 @@ import { DashboardComponent } from './dashboard.component';
       BrowserModule,
       FormsModule,
       HttpModule,
-      AppRoutingModule
+      AppRoutingModule,
+      InMemoryWebApiModule.forRoot(InMemoryDataService),
       ],
     declarations: [
       AppComponent,
